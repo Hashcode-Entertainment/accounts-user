1f9812ce12a51de678c8c093b1bc17cc3104f57e
Piotr Lyszkowski <tyrontundrom@gmail.com>
2022-05-20 13:54:49 +0200

    Initial commit

---
+++ src/main/java/com/sages/accountsuser/AccountsUserApplication.java
@@ -0,0 +1,13 @@
+package com.sages.accountsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sages/accountsuser/AccountsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sages.accountsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
