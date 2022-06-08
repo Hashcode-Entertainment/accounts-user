7336930020248beaa55b8068b4a80ccde67b3737
Piotr Lyszkowski <tyrontundrom@gmail.com>
2022-06-08 20:05:51 +0200

    change roles

---
+++ src/main/java/com/sages/accountsuser/user/domain/UsersRole.java
@@ -0,0 +1,22 @@
+package com.sages.accountsuser.user.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Getter
+class UsersRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Role name;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+}
--- src/main/java/com/sages/accountsuser/user/service/UserService.java
+++ src/main/java/com/sages/accountsuser/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.sages.accountsuser.user.domain.User;
 import com.sages.accountsuser.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
