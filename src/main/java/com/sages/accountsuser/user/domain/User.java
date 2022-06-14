9029ee338b6c3726d45e35a4de11a5225f236a45
Sebastian Roslon <sebekk7@gmail.com>
2022-06-14 17:36:03 +0200

    refactoring

--- src/main/java/com/sages/accountsuser/user/domain/User.java
+++ src/main/java/com/sages/accountsuser/user/domain/User.java
@@ -5,16 +5,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import java.util.Collection;
 
 @Entity
 @Getter
@@ -23,7 +20,7 @@ import java.util.Collection;
 @ToString
 @Builder
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,40 +29,11 @@ public class User implements UserDetails {
     private String lastName;
     private String password;
     private String email;
-//    @Enumerated - zakomentowane bo inaczej nie staruje aplikacja
-    private Enum role;
+    //- zakomentowane bo inaczej nie staruje aplikacja
+    @ManyToOne
+    private UsersRole role;
     private String information;
 
     public User() {
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
     }
 }
